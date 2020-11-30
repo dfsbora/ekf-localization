79f9d54c3849656b2b05c0c8d5c5d7326dd254a9
dfsbora <dfsbora@gmail.com>
2020-11-29 22:00:14 -0300

    feat: LandMark detector test thread main module imports another module which contains a class

--- v6_localization/teste.py
+++ v6_localization/teste.py
@@ -13,19 +13,5 @@ import modulo
 session = qi.Session()
 def main():
 
-    modulo.run(session)
+    modulo.main(session)
 
-    # mem_service = session.service("ALMemory")
-    # lmark_service = session.service("ALLandMarkDetection")
-
-    # try:
-    #     while True:
-    #         val = mem_service.getData("LandmarkDetected", 0)
-    #         print("val ", val)
-    #         time.sleep(0.5)
-    #         print("\n")
-    # except KeyboardInterrupt:
-    #     print "Interrupted by user, stopping LandmarkDetector"
-    #     self.landmark_service.unsubscribe("LandmarkDetector")
-    #     #stop
-    #     sys.exit(0)
